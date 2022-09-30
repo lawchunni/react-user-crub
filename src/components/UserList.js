bbf33d77fcf583ff9eb5a2b251c15286f9cd7059
Pui Ching Chung <lawchunni@yahoo.com.hk>
2022-09-30 14:40:14 -0500

    html refactored

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -18,9 +18,9 @@ const UserList = ({error, isLoaded, users, handleUserAction}) => {
   }
 
   if(error) {
-    return <div>Error: {error.message}</div>;
+    return <div className="container is-max-desktop">Error: {error.message}</div>;
   } else if (!isLoaded) {
-    return <div>Loading...</div>
+    return <div className="container is-max-desktop">Loading...</div>
   } else {
     return (
       <div className="container is-max-desktop">
